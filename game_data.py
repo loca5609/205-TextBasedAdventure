4cb101a5a150c5816919b8b74ae6d0afd7fa33cd
ああああああああ\Yayoi <no>
2018-12-16 10:38:00 -0800

    merged from angelos branch

--- game_data.py
+++ game_data.py
@@ -2,7 +2,6 @@ world_items = {
     # "dict_key": string with spaces (the dictionary key of the Item object),
     # "name": string (name of the item),
     # "item_desc": string (dialogue shown when Inspecting the item),
-    # "danger": boolean (if true, go to game over state. In game over state, check what item or room danger boolean = true, and display that specific game over message. (specific messages probably in another dictionary))
     # "effect": string/none (what effect this item does when Inspect, other than the default inspect function)
 
     ### BIG LEVER (key)
@@ -13,30 +12,31 @@ world_items = {
         "In the top most drawer lies a piece of paper. A note, with only '0413' inscribed on it.",
 
     ### PILES OF PAPER (no effect)
-    "piles of paper": "The papers are shredded. The pages of fanfiction won’t here won’t help you here.",
+    "piles of paper": "The papers are shredded. The pages of fanfiction won’t help you here.",
 
     ### WOODEN CHAIR (no effect)
-    "wooden chair": "You try sitting in the chair." 
+    "wooden chair": "You try sitting in the chair. " 
         "You now have uncomfortable splinters in unforetold places.",
 
     ### POOLS OF WATER (no effect)
-    "pools of water": "Upon further inspection, certain chemicals from your previous experiments have leaked through the VENTS and drains." 
+    "pools of water": "Upon further inspection, certain chemicals from your previous experiments have leaked through the VENTS and drains. " 
         "How? You don’t know. But this is a problem.",
 
-    ### chekhov'S GUN (danger, key item)
+    ### CHEKHOV'S GUN (danger, key item)
     "chekhov's gun": {
         "item_desc": "You hold the gun in your hands. The most powerful weapon in the entire laboratory. Nostalgically, you remember that it "
-            "only shoots modified METAL BULLETS of your own design. Of course, nothing in the entire world would ever require its "
+            "only shoots modified METAL BULLETs of your own design. Of course, nothing in the entire world would ever require its "
             "immense power. You carefully drop it in your HAMMERSPACE anyway.",
         "effect": "inspect_prereq",
         "prereq": "hazard suit",
-        "death_msg": "Wading through the pools of water, you begin to realize that the radiation is too much for your feeble body can handle. GAME OVER."
+        "death_msg": "Wading through the pools of water, you begin to realize that the radiation is too much for your feeble body to handle. GAME OVER."
     },
 
-    ### WOODEN FURNITURE ## HAS METAL BULLET QUESTION
+    ### WOODEN FURNITURE ## HAS METAL BULLET
+    # "new_item": This particular item has its flags changed, rather than the item inspected
     "wooden furniture": {
-        "item_desc": "You once handcrafted all your tables and chairs with your grandchildren many summers ago. It is now beyond repair." 
-        "Amid the destruction, you find a single modified METAL BULLET."
+        "item_desc": "You once handcrafted all your tables and chairs with your grandchildren many summers ago. It is now beyond repair. " 
+        "Amid the destruction, you find a single modified METAL BULLET. "
         "Old treasures are hard to come by, you stow it in your HAMMERSPACE.",
         "effect": "hammerspace_diff_item",
         "new_item": "metal bullet"
@@ -57,25 +57,26 @@ world_items = {
     "viscous_liquid": "Trails of the liquid lead from tubes to the VENTS. You need to find her fast.",
 
     ### VENTS
-    "vents": "You open the grate to the VENTS, but you are too large to fit inside."  
+    "vents": "You open the grate to the VENTS, but you are too large to fit inside. "  
         "SHE could be anywhere in the lab now.",
 
-    ### HAZARD SUITS (key item)
+    ### HAZARD SUIT (key item)
     "hazard suit": "You always felt safe in one of these. Better put it on.",
 
     ### WET THUD (danger)
-    "wet thud": {
-        "item_desc": "The sound you make in turning around alerts HER to your presence. In her manic rampage, she shreds you to pieces. GAME OVER.",
-        "effect": "inspect_kill"
-    },
+    # "wet thud": {
+    #     "item_desc": "The sound you make in turning around alerts HER to your presence. In her manic rampage, she shreds you to pieces. GAME OVER.",
+    #     "effect": "inspect_kill"
+    # },
 
-    ### WET FOOTSTEPS (danger)
-    "wet footsteps": {
-        "item_desc": "The sound you make in turning around alerts HER to your presence. In her manic rampage, she shreds you to pieces. GAME OVER.",
-        "effect": "inspect_kill"
-    }
+    # ### WET FOOTSTEPS (danger)
+    # "wet footsteps": {
+    #     "item_desc": "The sound you make in turning around alerts HER to your presence. In her manic rampage, she shreds you to pieces. GAME OVER.",
+    #     "effect": "inspect_kill"
+    # }
 }
 
+# Flags of important items the player must visit in order to progress.
 item_flags = {
     # "name of item": boolean (Player has or hasn't visited this key item)
     
@@ -86,7 +87,12 @@ item_flags = {
     "metal desk": False
 }
 
+# Flags of important items the player must visit in order to progress.
+# Distinguished from item_flags, as items in the hammerspace can be reviewed
+# with the "inventory" command.
 hammerspace_flags = {
+    # "name of item": boolean (Player has or hasn't visited this key item)
+
     ### CHEKHOV'S GUN (danger, key item)
     "chekhov's gun": False,
 
@@ -100,28 +106,30 @@ hammerspace_flags = {
 world_rooms = {
     # '''
     # "dict_key": snake_case_string (the dictionary key of the Room object),
-    # "title": string (Title of the room that player will see),
-    # "room_desc": string (description of the room),
-    # "connections": array of strings (keys of other Room objects that the Player object can enter),
-    # "key_item": string/None (key of item that must be visited in order to enter this Room, or None if no Item is required),
-    # "items": string/None (keys of items that player can interact with inside this Room, or None if no Items exist in room),
-    # "url": None (URL of the image associated with room; you do not need to enter a value here)
+    #   "title": string (Title of the room that player will see),
+    #   "room_desc": string (description of the room) or array of strings (locked, then unlocked descriptions)
+    #   "connections": array of strings (keys of other Room dicts that the Player can enter)
+    #   
+    #   OTHER ROOM PARAMETERS
+    #   "items": array of strings (Items the player can interact with)
+    #   "key_item": string (Key for item that exists in one of the flag dictionaries, necessary for progress)
+    #   "inspect_key": boolean (Needed to distinguish if a locked room has something that can be inspected to unlock it)
     # '''
     ### LABORATORY ANTECHAMBER
     "laboratory antechamber": {
         "title" : "Laboratory Antechamber",
         "room_desc" : "You enter your LABORATORY ANTECHAMBER. " 
             "It is as clean and organized as you had left it, perfect for patients to wait in. " 
-            "Judging from the blaring red lights, the whole building is in LOCKDOWN. " 
-            "This will not be as straightforward as you had hoped."
-            "There is a door leading to a long PASSAGE to THE STUDY.",
+            "Judging from the blaring red lights, the whole building is in lockdown. " 
+            "This will not be as straightforward as you had hoped. "
+            "There is a door leading to a long PASSAGE towards the rest of the building.",
         "connections" : ["passage"]
     },
 
     ### PASSAGE TO THE STUDY  
     "passage": {
         "title" : "30 ft Passage",
-        "room_desc" : "The PASSAGE continues for another 30 feet. Sirens blare loudly."
+        "room_desc" : "The PASSAGE continues for another 30 feet. Sirens blare loudly. "
             "Along the wall there are several doors. "
             "One door leads to the CALITIS OFFICE, another to the CISTERN, and the last to the STUDY.",
         "connections" : ["laboratory antechamber", "calitis office", "cistern", "study"]
@@ -130,9 +138,9 @@ world_rooms = {
     ### MR. CALITIS OFFICE
     "calitis office": {
         "title" : "The Calitis' Office",
-        "room_desc" : "You notice CLAW MARKS leading to the office door. " 
-            "Inside, the neatly stacked PILES OF PAPER are all shredded and seemingly tossed wildly across the room." 
-            "The WOODEN CHAIR is in pieces. The only thing left intact is the METAL DESK. ",
+        "room_desc" : "You notice claw marks leading to the office door. " 
+            "Inside, the neatly stacked PILES OF PAPER are all shredded and seemingly tossed wildly across the room. " 
+            "The WOODEN CHAIR is in pieces. The only thing left intact is the METAL DESK.",
         "connections" : ["passage"],
         "items": ["metal desk", "piles of paper", "wooden chair"]
     },
@@ -140,22 +148,21 @@ world_rooms = {
     ### CISTERN
     "cistern": {
         "title" : "The Cistern",
-        "room_desc" : "The once pristine POOLS OF WATER in this room are now tainted with radioactive chemicals." 
+        "room_desc" : "The once pristine POOLS OF WATER in this room are now tainted with radioactive chemicals. " 
             "Probably the same that caused HER to freak out. On the opposite wall, across the POOLS OF WATER, hangs CHEKHOV’S GUN.",
         "connections" : ["passage"],
         "items": ["pools of water", "chekhov's gun"]
     },
 
-    ### THE STUDY ## Only has locked description
+    ### THE STUDY
     "study": {
         "title" : "The Study",
         "room_desc" : 
             #Locked Description
-            ["The door to THE STUDY is locked. The ALPHANUMERIC KEYPAD glows red angrily as you press random codes.", 
+            ["The door to the STUDY is locked. The alphanumeric keypad glows red angrily as you press random codes.", 
             #Unlocked description
-            "Inside is a mess. The WOODEN FURNITURE is wrecked." 
-            "Splotches of GREEN VOMIT lay strewn across the walls and floor. There is a door leading to the EXPERIMENTATION LAB"
-            "What do you inspect?"],
+            "Inside is a mess. The WOODEN FURNITURE is wrecked. " 
+            "Splotches of GREEN VOMIT lay strewn across the walls and floor. There is a door leading to the EXPERIMENTATION LAB."],
         "connections" : ["passage", "experimentation lab"],
         "key_item": "metal desk",
         "items": ["wooden furniture", "green vomit",]
@@ -166,17 +173,22 @@ world_rooms = {
         "title" : "The Experimentation Lab",
         "room_desc" : 
             #Locked Description
-            ["The door to the labs is a heavy metal one. It’s locked." 
+            ["The door to the labs is a heavy metal one. It’s locked. " 
             "There are no knobs to turn, only one BIG LEVER on the side of the wall.", 
             #Unlocked Description
-            "There is glass everywhere. Lines of VISCOUS LIQUID streak from tubes along the floor." 
-            "The many HAZARD SUITS hang untouched in their cabinets. There is also the door to the MACGUFFIN ROOM."
-            "What do you inspect?"],
+            "There is glass everywhere. Lines of VISCOUS LIQUID streak from tubes along the floor. " 
+            "You see a HAZARD SUIT hang untouched in one of the closets. There is also the door to the MACGUFFIN ROOM."],
         "connections" : ["study", "macguffin room"],
         "key_item": "big lever",
         "inspect_key": True,
         "items": ["viscous liquid", "vents", "hazard suit"]
     },
+
+    # '''
+    #   "specific_item": string (The specific sequence that must be inspected in order to continue, or else GAME OVER)
+    #   "success_dest": string (Dictionary key for room upon successfully inspecting particular item)
+    #   "death_msg": dictionary of strings (Death messages for inspecting a particular thing, as well as a default death message)
+    # '''
 
     ### MACGUFFIN ROOM / MACGUFFIN 00
     "macguffin room": {
@@ -195,7 +207,7 @@ world_rooms = {
     ## MACGUFFIN 01
     "macguffin_01": {
         "title" : "5",
-        "room_desc" : "You don’t remember designing the security measures like this, but the MACGUFFIN is encased in an incredibly large box." 
+        "room_desc" : "You don’t remember designing the security measures like this, but the MACGUFFIN is encased in an incredibly large box. " 
             "Protecting the box, there is an ALPHANUMERIC KEYPAD and a LARGE LEVER. "
 
             "Since SHE is blind, SHE hasn’t noticed you yet. " 
@@ -267,7 +279,7 @@ world_rooms = {
     ### MACGUFFIN 05
     "macguffin_05": {
         "title" : "1",
-        "room_desc" : "You swing CHEKHOV'S GUN toward HER. Point. BANG!" 
+        "room_desc" : "You swing CHEKHOV'S GUN toward HER. Point. BANG! " 
             "The METAL BULLET flies through the air. You feel the recoil in your shoulder. " 
             "If you hadn’t been the original designer, your arm might have been ripped off. "
 
